0eba51f02317ebded9f9e820ed2332aaaf9fb355
sergysurge <sergysarkisyan@gmail.com>
2018-01-31 14:58:30 -0800

    cheerio setup, getting name and price, looking into SNS

--- index.js
+++ index.js
@@ -1,14 +1,45 @@
 var cheerio = require('cheerio');
 var rp = require('request-promise-native');
+// var $ = require('jQuery');
 
 var options = {
 	uri: "https://shop.bitmain.com/main.htm?lang=en",
 	transform: function(body){
-		console.log('$$$ start from cheerio $$$', body, '$$$ end from cheerio $$$')
+		// console.log('$$$ start from cheerio $$$', body, '$$$ end from cheerio $$$')
 		return cheerio.load(body);
 	}
 }
 
 rp(options)
-	.then(function($) {console.log('### start in rp##' ,$, '### end in rp###')})
-	.catch(function(err){console.error(err)})
+	.then(function($) {
+
+    $('#cartList li').each(function (index, li) {
+        var priceSTR = $(li).find('.price div[class*="currency"]').text();
+        var price = Number(priceSTR.replace(/\D/g, ''));
+
+        console.log(priceSTR, 'should be price', Number(priceSTR.replace(/\D/g, '')));
+
+        if (price > 2100) {
+          var name = $(li).find('.goodsDescrip h2').text();
+          var button = $(li).find('.button_set button[class!="learn"]').text();
+
+          console.log(name, button, 'name and button');
+
+          if (!/sold/i.test(button)) {
+          	console.log('good results, sending to chen', name + button)
+            // console.log(`Message we sent with ${name.textContent} and ${button.textContent}. We are ready for you`)
+          } else {
+          	console.log('SOLD OUT OF ', name + button)
+            // console.log(`OUT OF STOCK ${name.textContent} and ${button.textContent}.NOOO!!`)
+          }
+        }
+      })
+  })
+		// console.log('### start in rp##' ,$, '### end in rp###')})
+	.catch(function(err){console.error('its really an error', err, 'an error has occured')})
+
+//.button_set 2 buttons, not button class=learn butt class (add or sold)
+//if add then go if sold then no
+
+//.price .currency-USD-88    .text or .innerHTML
+
